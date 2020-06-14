624e2bdb74429e8587c4a18eb56e70420f4c2fb2
tko99999 <araitakao@arii-inc.com>
2020-06-14 17:28:54 +0900

    1.0.1

--- app/controllers/scraping_item_controller.rb
+++ app/controllers/scraping_item_controller.rb
@@ -4,6 +4,26 @@ class ScrapingItemController < ApplicationController
      page = agent.get('https://zozo.jp/')
      elements = page.search('title')
      puts elements
-     
+
+     #ブランド名取得
+     agent = Mechanize.new
+     page = agent.get("https://zozo.jp/shop/newbalance/goods-sale/41433459/?did=69286059&rid=1093")
+     elements = page.search('//*[@id=“nameList”]/li[2]/a') # ul要素の下のli要素を検索 xpathでコピーする
+     puts elements
+
+     #商品名取得
+     agent = Mechanize.new
+     page = agent.get("https://zozo.jp/shop/newbalance/goods-sale/41433459/?did=69286059&rid=1093")
+     elements = page.search('h1')
+     puts elements
+
+     #値段取得
+     agent = Mechanize.new
+     page = agent.get("https://zozo.jp/shop/newbalance/goods-sale/41433459/?did=69286059&rid=1093")
+     elements = page.search('div.goods-price discount-price') #ちょっとこれがあってるか不明
+     puts elements
+
+
+
    end
 end
---
+++ db/migrate/20200614082515_add_column_to_scraping_item.rb
@@ -0,0 +1,4 @@
+class AddColumnToScrapingItem < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
