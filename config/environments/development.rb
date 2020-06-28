2eeac05a48c30110a10c404e16daa965df9368a7
tko99999 <araitakao@arii-inc.com>
2020-06-28 14:18:14 +0900

    Initial Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #devise関連
+
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.default_url_options = { host: 'localhost:3000' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/index'
   get 'pages/show'
   get 'scraping_item/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root to: 'scraping_item#index'#
+
  resources :scraping_item
 
- #devise関連
+
  root 'pages#index'
   get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
