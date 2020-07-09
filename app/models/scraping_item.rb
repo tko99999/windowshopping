7340904e0ba6384670f09fe5295a7879494aab51
tko99999 <araitakao@arii-inc.com>
2020-07-10 00:22:23 +0900

    Initial Commit

--- app/models/scraping_item.rb
+++ app/models/scraping_item.rb
@@ -1,2 +1,8 @@
 class ScrapingItem < ApplicationRecord
+  def self.search(search) #self.はUser.を意味する
+     if search
+       where(['username LIKE ?', "%#{search}%"]) #検索とuseanameの部分一致を表示。
+     else
+       all #全て表示させる
+    end
 end
