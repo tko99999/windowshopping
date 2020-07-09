29642037967ef9fc67e10945a16992539427739b
tko99999 <araitakao@arii-inc.com>
2020-07-09 10:37:13 +0900

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  resources :contacts
   devise_for :users
   get 'pages/index'
   get 'pages/show'
-  get 'scraping_item/index'
   get 'scraping_item/search'
-  get "scraping_item/show" => "scraping_item/show"
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
  resources :scraping_item
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_142308) do
+ActiveRecord::Schema.define(version: 2020_07_06_150545) do
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "scraping_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "brandname"
